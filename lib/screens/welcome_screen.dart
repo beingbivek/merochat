99b550978764e5eb72407fb88e137ec6f0625abd
Bivek Thapa <beingbivek@gmail.com>
2021-05-02 10:12:16 +0545

    Custom Animation and Tween in the welcome page

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -21,6 +21,20 @@ class _WelcomeScreenState extends State<WelcomeScreen>
         AnimationController(vsync: this, duration: Duration(seconds: 1));
     animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
     controller.forward();
+    //Tween can be of any widget and its properties
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+
+    //For Animating back and forward
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    // });
+
+    // Important for changing the animation in screen
     controller.addListener(() {
       setState(() {});
       print(animation.value);
@@ -28,9 +42,15 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   }
 
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -43,7 +63,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: animation.value * 100,
+                    height: 60.0,
                   ),
                 ),
                 Text(
